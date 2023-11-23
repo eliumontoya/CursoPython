660ee2e999d0aa8261261eba4ccb1f7d07a9b8fb
eliumontoya <correo@eliumontoya.com>
2023-11-22 23:39:00 -0600

    agregando seccion 2.3

---
+++ essentials/operadores.py
@@ -0,0 +1,32 @@
+'''
+Created on 22 nov. 2023
+
+@author: eliumontoya
+'''
+print(True > False)
+print(True < False)
+
+
+
+## Redondeo hacia abajo
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+## Prioridades de potencia es al reves: de derecha a izq.
+print(2 ** 2 ** 3)
+
+
+
+## Ejercicios de prioridades
+
+print((2 ** 4), (2 * 4.), (2 * 4))
+
+
+
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+
+
+
+print((2 ** 4), (2 * 4.), (2 * 4))
